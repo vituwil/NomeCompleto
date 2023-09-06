03405c88f2faeadab7217e4d26971d64571e28f0
Victor <victorrefundine@hotmail.com>
2023-09-06 20:42:18 -0300

    Adição de programa

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿string nome, sobrenome;
+Console.Write("Digite o seu nome: ");
+nome = Console.ReadLine()!;
+
+Console.Write("Digite o seu sobrenome: ");
+sobrenome = Console.ReadLine()!;
+
+Console.WriteLine($"Nome completo: {nome} {sobrenome}");
+Console.WriteLine($"Nome de catálago: {sobrenome.ToUpper()}, {nome}");
